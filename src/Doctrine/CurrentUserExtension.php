d8cfb6e207eec3c0ba3c5770d60e867448c61e2b
cornel <cornel.siclovan@gmail.com>
2019-06-20 15:50:42 +0300

    CurrentUserExtension customized for getting the collection only for services that are listed in the service provider profile

--- src/Doctrine/CurrentUserExtension.php
+++ src/Doctrine/CurrentUserExtension.php
@@ -12,7 +12,11 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInter
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\ClientSubService;
+use App\Entity\User;
+use App\Entity\UserSubService;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
+use function sprintf;
 use Symfony\Component\Security\Core\Security;
 
 // Api Platform Extensions
@@ -22,10 +26,12 @@ use Symfony\Component\Security\Core\Security;
 final class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
     private $security;
+    private $entityManager;
 
-    public function __construct(Security $security)
+    public function __construct(Security $security, EntityManagerInterface $entityManager)
     {
         $this->security = $security;
+        $this->entityManager = $entityManager;
     }
 
 
@@ -41,14 +47,33 @@ final class CurrentUserExtension implements QueryCollectionExtensionInterface, Q
 
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
     {
-        if(ClientSubService::class !==  $resourceClass || $this->security->isGranted('ROLE_SERVICE_PROVIDER') || null === $user = $this->security->getUser())
+        /** @var User $user */
+        $user = $this->security->getUser();
+
+        /** @var UserSubService $user_services */
+        $user_services = $this->entityManager->getRepository(UserSubService::class)->findBy(["user"=>$user]);
+        $ids = [];
+
+        foreach ($user_services as $user_service) {
+            $ids[] = $user_service->getService()->getId();
+        }
+
+        if(ClientSubService::class !==  $resourceClass || null === $user = $this->security->getUser())
         {
             return;
         }
 
-        $rootAlias = $queryBuilder->getRootAliases()[0];
 
-        $queryBuilder->andWhere(sprintf('%s.user = :current_user', $rootAlias));
-        $queryBuilder->setParameter('current_user', $user);
+        if(!$this->security->isGranted('ROLE_SERVICE_PROVIDER') && null !== $user){
+            $rootAlias = $queryBuilder->getRootAliases()[0];
+            $queryBuilder->andWhere(sprintf('%s.user = :current_user', $rootAlias));
+            $queryBuilder->setParameter('current_user', $user);
+        }
+
+        if($this->security->isGranted('ROLE_SERVICE_PROVIDER')){
+            $rootAlias = $queryBuilder->getRootAliases()[0];
+            $queryBuilder->andWhere(sprintf('%s.service in (:ids)', $rootAlias));
+            $queryBuilder->setParameter('ids', $ids);
+        }
     }
 }
